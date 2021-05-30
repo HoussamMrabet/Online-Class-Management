fef61c4b99abef8e6a23ff807580f8ef714cb618
HoussamMrabet <dodslim100@gmail.com>
2021-05-31 00:58:14 +0100

    login

--- SchoolProjectS6/PFES6/views.py
+++ SchoolProjectS6/PFES6/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from .models import Users
-from .forms import UsersForm
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User,auth
-
-# Create your views here.
-
-
 
 # Create your views here.
 
@@ -25,9 +16,7 @@ def teacher_index(request):
     return render(request, 'Teacher/teacherSpace.html')
 
 
-@login_required
 def user_logout(request):
-    logout(request)
     return redirect('PFES6:index')
 
 
@@ -36,25 +25,15 @@ def user_login(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-      
-        user = auth.authenticate(email=email, password=password)
 
-        if user is not None:
-            login(request, user)
-            userInfo = Users.objects.get(email=email)
-            if userInfo.role == 'student':
-                return render(request, 'student/index.html', {'user': userInfo})
+        user = Users.objects.get(email=email)
 
-            elif userInfo.role == 'teacher':
-                return render(request, 'Teacher/teacherSpace.html', {'user': userInfo})
-
+        if user is not None and password == user.password:
+            if user.role == 's':
+                return render(request, 'student/index.html', {'user': user})
             else:
-                return redirect('PFES6:index')
+                return render(request, 'teacher/teacherSpace.html', {'user': user})
         else:
-            return redirect('PFES6:index')
+            return render(request, 'index.html')
     else:
-
         return render(request, 'index.html')
-
-       
-
